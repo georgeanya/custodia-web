7f16418957ad2193b3f4d7d95fbd78673baa39b9
George Anya <georgeanya4real@gmail.com>
2024-11-20 17:00:05 +0100

    commit

--- components/blog.tsx
+++ components/blog.tsx
@@ -28,18 +28,14 @@ const SustainOutlineButton = styled(Button)({
 const SustainButton = styled(Button)({
   background: "#4F9EEA !important",
   fontFamily: "Circular Std",
-  fontSize: "15px",
-  lineHeight: "18.97",
   color: "#f8f8f8",
   cursor: "pointer",
-  padding: "18.5px 30px",
+  padding: "20px 30px",
+  margin: "0px 0px",
   borderRadius: "32px",
   textTransform: "none",
-  fontWeight: 500,
   ["@media (max-width:780px)"]: {
     padding: "16px 30px",
-    fontSize: "14px",
-    lineHeight: "17.71",
   },
 });
 
