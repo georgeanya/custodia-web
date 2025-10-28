26a48ba26ba9f778ec26f7b871946b0deb8c4e74
georgeanya <georgeanya4real@gmail.com>
2025-10-28 23:10:14 +0100

    sitemap update

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,7 +1,7 @@
 const SITE_URL = 'https://custodiahealth.com'
 
 function generateSiteMap(posts) {
-    return `<?xml version="1.0" encoding="UTF-8"?>
+  return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <url>
        <loc>${SITE_URL}</loc>
@@ -22,7 +22,7 @@ function generateSiteMap(posts) {
        <priority>0.8</priority>
      </url>
      <url>
-       <loc>${SITE_URL}/pricing/loc>
+       <loc>${SITE_URL}/pricing</loc>
        <lastmod>${new Date().toISOString()}</lastmod>
        <changefreq>monthly</changefreq>
        <priority>0.8</priority>
@@ -58,8 +58,8 @@ function generateSiteMap(posts) {
        <priority>0.8</priority>
      </url>
      ${posts
-            .map((post) => {
-                return `
+      .map((post) => {
+        return `
        <url>
          <loc>${SITE_URL}/blog/${post.attributes.slug}</loc>
          <lastmod>${post.attributes.updatedAt}</lastmod>
@@ -67,28 +67,28 @@ function generateSiteMap(posts) {
          <priority>0.8</priority>
        </url>
      `
-            })
-            .join('')}
+      })
+      .join('')}
    </urlset>
  `
 }
 
 export async function getServerSideProps({ res }) {
-    const response = await fetch('https://custodia-health-blog.herokuapp.com/api/articles?pagination[pageSize]=500&fields[0]=title&fields[1]=slug&fields[2]=updatedAt')
-    const data = await response.json()
-    const posts = data.data
+  const response = await fetch('https://custodia-health-blog.herokuapp.com/api/articles?pagination[pageSize]=500&fields[0]=title&fields[1]=slug&fields[2]=updatedAt')
+  const data = await response.json()
+  const posts = data.data
 
-    const sitemap = generateSiteMap(posts)
+  const sitemap = generateSiteMap(posts)
 
-    res.setHeader('Content-Type', 'text/xml')
-    res.write(sitemap)
-    res.end()
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(sitemap)
+  res.end()
 
-    return {
-        props: {},
-    }
+  return {
+    props: {},
+  }
 }
 
 export default function Sitemap() {
-    return null
+  return null
 }
